2223db39431cbcd3e4b73b159a44ea5e260c6768
Yassine AZZOUT <yassine.azzout@gmail.com>
2020-03-09 10:56:13 +0100

    tests(convert): complete first test from usd to eur

--- currency/index.test.js
+++ currency/index.test.js
@@ -1,3 +1,4 @@
+const currency = require('./');
 const nock = require('nock');
 
 beforeEach(() => {
@@ -41,9 +42,9 @@ beforeEach(() => {
 
 describe('currency', () => {
   test('should convert 1 USD to EUR', async () => {
-    throw new Error(
-      'test not yet defined... remove the throw and write your test here'
-    );
+    const converted = await currency({'amount': 1, 'from': 'USD', 'to': 'EUR'});
+
+    expect(converted).toBe(0.899);
   });
 
   test('should convert 1 USD to USD', async () => {
